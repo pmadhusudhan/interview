a96bf3587c9dff6009cc9b5130c82175f5fdec4c
pmadhusudhan <madhutemp@gmail.com>
2023-02-19 19:57:36 -0500

    Create chat.js

---
+++ chat.js
@@ -0,0 +1,6 @@
+/*
+
+javascript: s = document.createElement('script');s.src='https://pmadhusudhan.github.io/interview/chat.js';document.getElementsByTagName("body")[0].appendChild(s);
+
+*/
+alert("Inserted js");
