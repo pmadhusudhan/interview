cf799acac6acc7127d3694477ce853cd58c5c8e7
pmadhusudhan <madhutemp@gmail.com>
2023-02-19 20:34:57 -0500

    Update chat.js

--- chat.js
+++ chat.js
@@ -3,9 +3,4 @@
 javascript: s = document.createElement('script');s.src='https://pmadhusudhan.github.io/interview/chat.js';document.getElementsByTagName("body")[0].appendChild(s);
 
 */
-javascript: s = document.createElement('script');s.src='https://code.jquery.com/jquery-3.6.3.min.js';document.getElementsByTagName("body")[0].appendChild(s);
-
-$("document").ready(function(){
-Console.log($("body").length);
-  
-});
+$$("body").append("<div id='popup'
