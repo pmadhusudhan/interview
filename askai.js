52d385b82d14ade658f741bb70656796d517c7d6
pmadhusudhan <madhutemp@gmail.com>
2023-02-22 15:19:14 -0500

    Update askai.js

--- askai.js
+++ askai.js
@@ -30,7 +30,7 @@ if(window.location.hostname === "finviz.com"){
     var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
      var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
           var s = innerDoc.createElement("script");
-            s.innerHTML = "function replaceq(question){t = innerDoc.body.getElementsByTagName("textarea")[0].value;t = question + ":"+ t.substring(t.indexOf(':')+1);innerDoc.body.getElementsByTagName("textarea")[0].value =t;}";
+            s.innerHTML = "function replaceq(question){t = innerDoc.body.getElementsByTagName('textarea')[0].value;t = question + ':'+ t.substring(t.indexOf(':')+1);innerDoc.body.getElementsByTagName('textarea')[0].value =t;}";
             innerDoc.body.appendChild(s);   
         var a = innerDoc.createElement("button");
             a.setAttribute("onclick","replaceq('Give me questions based on this')");
