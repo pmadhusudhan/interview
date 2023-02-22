6147ccb6df489a91c55e4635a294a399bd7d9904
pmadhusudhan <madhutemp@gmail.com>
2023-02-22 10:59:03 -0500

    Update askai.js

--- askai.js
+++ askai.js
@@ -32,7 +32,23 @@ if(window.location.hostname === "finviz.com"){
     innerDoc.body.getElementsByTagName("textarea")[0].value = value;
    // });
 }
+if(window.location.hostname === "youtube.com"){
+    var t= $("#below").text();
+        //console.log(t)
 
-  
+        var q = "Identify the topic and Summarize in max 100 words:";
+        $('body').click(function(){
+        var value = q + $("#below").text();
+        //console.log(value);
+        var iframe = document.getElementsByTagName("iframe")['chatgpt-everywhere-iframe-7cbe6781-4d56-4425-8985-23b903e3d74c'];
+        iframe.setAttribute("style","width:300px;height:90%");
+
+
+        var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+        innerDoc.body.getElementsByTagName("textarea")[0].value = value;
+        innerDoc.body.getElementsByTagName("button")[1].click();
+        });
+}
   console.log("askai.js loaded");
 });
