eb1f6a3ac5589040397c2cd6023d8008225f17b2
pmadhusudhan <madhutemp@gmail.com>
2023-02-22 09:49:40 -0500

    Update askai.js

--- askai.js
+++ askai.js
@@ -1,9 +1,11 @@
 var q = "List 10 key skills from this:";
-$('body').click(function(){
+$('document').ready(function(){
+  $('body').click(function(){
 var value = q + $(".jobs-description").text();
 console.log(value);
 var iframe = document.getElementsByTagName("iframe")['chatgpt-everywhere-iframe-7cbe6781-4d56-4425-8985-23b903e3d74c'];
 var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
 
 innerDoc.body.getElementsByTagName("textarea")[0].value = value;
+  });
 });
