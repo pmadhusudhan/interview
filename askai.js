dcf08af85273b20f1fd0f6b61c19b60a4b4db800
pmadhusudhan <madhutemp@gmail.com>
2023-02-22 10:48:46 -0500

    Update askai.js

--- askai.js
+++ askai.js
@@ -1,3 +1,13 @@
+function defer(method) {
+    if (window.jQuery) {
+        method();
+    } else {
+        setTimeout(function() { defer(method) }, 50);
+    }
+}
+
+defer(function () {
+   
 if(window.location.hostname === "linkedin.com"){
 var q = "List 10 key skills from this:";
 
@@ -13,13 +23,16 @@ innerDoc.body.getElementsByTagName("textarea")[0].value = value;
 
 if(window.location.hostname === "finviz.com"){
     var q = "What is driving the market based on these news:";
-    $('body').click(function(){
+   // $('body').click(function(){
     var value = q + $("table").text();
     console.log(value);
     var iframe = document.getElementsByTagName("iframe")['chatgpt-everywhere-iframe-7cbe6781-4d56-4425-8985-23b903e3d74c'];
     var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
 
     innerDoc.body.getElementsByTagName("textarea")[0].value = value;
-    });
+   // });
 }
 
+  
+   alert("jQuery is now loaded");
+});
