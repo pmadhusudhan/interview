f6aa9e1bd6c01f250230cc4a2fe862485647c415
pmadhusudhan <madhutemp@gmail.com>
2023-03-03 09:28:17 -0500

    Delete chat.js

--- chat.js
+++
@@ -1,6 +0,0 @@
-/*
-
-javascript: s = document.createElement('script');s.src='https://pmadhusudhan.github.io/interview/chat.js';document.getElementsByTagName("body")[0].appendChild(s);
-
-*/
-$$("body").append("<div id='popup'
