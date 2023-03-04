7e31af55b73e6a45d92996417e788af4342e85a4
pmadhusudhan <madhutemp@gmail.com>
2023-03-03 19:56:46 -0500

    Update aipop.js

--- aipop.js
+++ aipop.js
@@ -1,7 +1,17 @@
+
+
+if(window.location.href.indexOf("google.com") >= 0){
 var a = document.getElementsByTagName("h3");//document.getElementsByClassName("MjjYud")[0].innerText;
 var content ="";
 for(i=0;i<a.length;i++){ content += a[i].innerText;};
 //console.log(content);
+  
+}
+if(window.location.href.indexOf("indeed.com") >= 0){
+  var content = document.getELementsByClassName("jobsearch-RightPane")[0].innerText;
+  
+}
+
 var ifr = document.createElement("iframe");
 ifr.setAttribute("style","position:fixed;bottom:0px;z-index:999999;right:0px;width:400px;height:50%;overflow:auto;background:white");
 ifr.src="https://pmadhusudhan.github.io/interview/askai.html?AskAI=summarize this:"+content;
