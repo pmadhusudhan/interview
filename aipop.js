10c28909cf00e6b1ba473fbc2c12a2b170d8fc1c
pmadhusudhan <madhutemp@gmail.com>
2023-03-03 19:51:16 -0500

    Update aipop.js

--- aipop.js
+++ aipop.js
@@ -1,6 +1,7 @@
-var content = $("h3").text();//document.getElementsByClassName("MjjYud")[0].innerText;
-
-console.log(content);
+var a = document.getElementsByTagName("h3");//document.getElementsByClassName("MjjYud")[0].innerText;
+var content ="";
+for(i=0;i<a.length;i++){ content += a[i].innerText;};
+//console.log(content);
 var ifr = document.createElement("iframe");
 ifr.setAttribute("style","position:fixed;bottom:0px;z-index:999999;right:0px;width:400px;height:50%;overflow:auto;background:white");
 ifr.src="https://pmadhusudhan.github.io/interview/askai.html?AskAI=summarize this:"+content;
