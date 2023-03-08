ed938a5ddf0c40c4a71423286eafe779f01fe7a4
pmadhusudhan <madhutemp@gmail.com>
2023-03-08 09:30:51 -0500

    Update jdres.js

--- jdres.js
+++ jdres.js
@@ -2,3 +2,23 @@ var d =document.createElement("div");
 d.innerHTML = "hello"
 d.setAttribute("style","position:fixed;top:0px;width:300px;height:100%;background:white;right:0px");
 document.getElementsByTagName("body")[0].appendChild(d);
+
+// create a new textarea element
+const textarea = document.createElement('textarea');
+// set attributes for the textarea element
+textarea.setAttribute('id', 'myTextarea');
+textarea.setAttribute('name', 'myTextarea');
+textarea.setAttribute('cols', '50');
+textarea.setAttribute('rows', '5');
+// append the textarea element to the body of the HTML page
+document.body.appendChild(textarea);
+
+// create a new button element
+const button = document.createElement('button');
+// set attributes for the button element
+button.setAttribute('id', 'myButton');
+button.setAttribute('name', 'myButton');
+button.innerHTML = 'Compare'; // set the label for the button
+// append the button element to the body of the HTML page
+document.body.appendChild(button);
+
