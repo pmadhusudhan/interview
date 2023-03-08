ea34cc0fbb8fd21c0e96ebbb5674493bd2edcbab
pmadhusudhan <madhutemp@gmail.com>
2023-03-08 10:33:08 -0500

    Update jdres.js

--- jdres.js
+++ jdres.js
@@ -1,6 +1,6 @@
 var d =document.createElement("div");
 d.innerHTML = "<textarea id='resume' cols='50' rows='5' placeholder='Paste your resume text here'></textarea><br><textarea id='jd' cols='50' rows='5' placeholder='Paste a job description here'></textarea><br><input id='question' value='Compare resume with Job description and list keywords and skills matching and missing'/><br><button id='compare'>Compare</button>"
-d.setAttribute("style","position:fixed;top:0px;width:400px;padding:10px;height:100%;background:white;right:0px");
+d.setAttribute("style","position:fixed;top:0px;width:400px;padding:10px;height:100%;background:#333;right:0px");
 document.getElementsByTagName("body")[0].appendChild(d);
 
 const button = document.querySelector('#compare');
