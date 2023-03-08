5d5f0326f200178f82029acfa56475108f36d1f3
pmadhusudhan <madhutemp@gmail.com>
2023-03-08 09:22:16 -0500

    Create jdres.js

---
+++ jdres.js
@@ -0,0 +1,4 @@
+var d =document.createElement("div");
+d.innerHTML = "hello"
+d.setAttribute("style","position:fixed;top:0px;width:300px;height:100%;background:white;right:0px");
+document.getElementsByTagName("body")[0].appendChild(d);
