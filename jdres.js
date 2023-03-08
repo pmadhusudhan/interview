6b77b3f00d562a82a0c814198b0b6421743ffcf5
pmadhusudhan <madhutemp@gmail.com>
2023-03-08 09:55:03 -0500

    Update jdres.js

--- jdres.js
+++ jdres.js
@@ -1,13 +1,16 @@
 var d =document.createElement("div");
-d.innerHTML = "<textarea id='resume' cols='50' rows='5'></textarea><br><button id='compare'>Compare</button>"
+d.innerHTML = "<textarea id='resume' cols='50' rows='5' placeholder='Paste your resume text here'></textarea><br><button id='compare'>Compare</button>"
 d.setAttribute("style","position:fixed;top:0px;width:300px;height:100%;background:white;right:0px");
 document.getElementsByTagName("body")[0].appendChild(d);
 
 const button = document.querySelector('#compare');
 
 function handleClick() {
-  console.log('Button clicked!');
-  document.getElementsByTagName("textarea")[0].value = "enter this question";
+  
+  question = "List keywords and skills from this resume :";
+  document.getElementsByTagName("textarea")[0].value = question + document.getElementById("resume").value ;
+
+  document.getElementsByTagName("button")[5].click()
 }
 
 button.addEventListener('click', handleClick);
