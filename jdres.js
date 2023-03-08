636d00c49294bd14284072002fbe5a29fba7ec6c
pmadhusudhan <madhutemp@gmail.com>
2023-03-08 10:52:13 -0500

    Update jdres.js

--- jdres.js
+++ jdres.js
@@ -1,5 +1,5 @@
 var d =document.createElement("div");
-d.innerHTML = "<textarea id='resume' cols='50' rows='5' placeholder='Paste your resume text here'></textarea><br><textarea id='jd' cols='50' rows='5' placeholder='Paste a job description here'></textarea><br><input id='question' value='Compare resume with Job description and list keywords and skills matching and missing'/><br><button id='compare'>Compare</button>"
+d.innerHTML = "<textarea id='resume' cols='30' rows='5' placeholder='Paste your resume text here'></textarea><br><textarea id='jd' cols='30' rows='5' placeholder='Paste a job description here'></textarea><br><input id='question' value='Compare resume with Job description and list keywords and skills matching and missing'/><br><button id='compare' style='border:solid 1px black;padding:5px;background:#fbe69e;border-radius:10px'>Compare</button>"
 d.setAttribute("style","position:fixed;top:0px;width:400px;padding:10px;height:100%;background:#333;right:0px");
 document.getElementsByTagName("body")[0].appendChild(d);
 
