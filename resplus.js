9496597049e7c38fb4ea48272bd39e12ec176689
pmadhusudhan <madhutemp@gmail.com>
2023-03-11 22:48:30 -0500

    Create resplus.js

---
+++ resplus.js
@@ -0,0 +1,10 @@
+  var questions = ["List all the linkedin skills based on this ","List suitable job titles for this resume"];
+  var jd_questions = [
+                      "What are key activities and responsibilities for this job",
+                      "How much salary can I expect for this job",
+                      "List 10 interview questions based on this job ",
+                      "Give me best candidate’s resume for inspiration based on this job",
+                      "What is not in my resume / experience based on this job",
+                      "What would be career path for this job", 
+                      "Which training and Certifications would help for this job"
+                  ]; 
