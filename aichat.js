53209e9a082c056c3764670567d556d8c1458689
pmadhusudhan <madhutemp@gmail.com>
2023-04-29 09:55:36 -0400

    aisidebar

--- aichat.js
+++ aichat.js
@@ -29,14 +29,14 @@ $(document).ready(function(){
 
 
 
-            $("body").append("<div id='sidebar' style='position:fixed;top:0px;right:0px;width:300px;height:100%;background:#888;padding:5px;z-index:99999999'></div>");
+            $("body").append("<div id='aisidebar' style='position:fixed;top:0px;right:0px;width:300px;height:100%;background:#888;padding:5px;z-index:99999999'></div>");
             $("html").css("width","calc(100% - 300px)");
 
-            $("#sidebar").append("<div id='container' style='background:#ddd; height: calc(100% - 120px);overflow:auto;padding: 10px;border:2px inset gray;border-radius:10px;margin:5px'><div style='background:#f6f6f4;border:1px solid gray;width:auto;border-radius:10px;padding:4px;bottom:0px'>Hello you can talk to me</div></div>");
-            $("#sidebar").append("<input id='questionbox' style='height:50px;width:280px;background:white;position:fixed;bottom:10px;border-radius:10px;padding:4px;' placeholder='Ask me' />");
+            $("#aisidebar").append("<div id='aicontainer' style='background:#ddd; height: calc(100% - 120px);overflow:auto;padding: 10px;border:2px inset gray;border-radius:10px;margin:5px'><div style='background:#f6f6f4;border:1px solid gray;width:auto;border-radius:10px;padding:4px;bottom:0px'>Hello you can talk to me</div></div>");
+            $("#aisidebar").append("<input id='questionbox' style='height:50px;width:280px;background:white;position:fixed;bottom:10px;border-radius:10px;padding:4px;' placeholder='Ask me' />");
 
 
-            $("#container").append("<img id='waiting' src='https://cdn.dribbble.com/users/2973561/screenshots/5757826/media/221d6bfc1960ab98a7585fcc2a4d0181.gif' style='width:50px;display:none;position:fixed;z-index:9999999;bottom:100px;' />");
+            $("#aicontainer").append("<img id='waiting' src='https://cdn.dribbble.com/users/2973561/screenshots/5757826/media/221d6bfc1960ab98a7585fcc2a4d0181.gif' style='width:50px;display:none;position:fixed;z-index:9999999;bottom:100px;' />");
             $('#questionbox').keypress(function(event) {
             if (event.which === 13) {
             
