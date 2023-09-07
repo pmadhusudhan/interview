d0a3f23ff0992b8b6dc8aaaed603eee964d9b059
pmadhusudhan <madhutemp@gmail.com>
2023-09-07 16:09:06 -0400

    Create prompts.js

---
+++ prompts.js
@@ -0,0 +1,8 @@
+var prompts = [["Summarize","Summarize with key take aways and list 5 follow up questions."],
+["2 min ","Summarize for 2min read."],
+["5 min","Rewrite for a 5min read."],
+["FAQ","Create a FAQ based on this."],
+["Simplify","Create a version of this document using plain understandable language."],
+["LinkedIn","Create a linkedin post highlighting the key takeaways from this."],
+["Spanish","Translate into spanish."]
+];
